3891987796eb50a45c871832f9f82c2edcedfb6f
Alwaz Shahid <52915358+Alwaz18@users.noreply.github.com>
2023-05-14 15:53:31 +0500

    encrypted

--- client.py
+++ client.py
@@ -1,19 +1,28 @@
 import socket
 import threading
+import os
+from cryptography.fernet import Fernet
 
-HOST = '127.0.0.1'  # IP address of the host machine
-PORT = 5000  # Port to connect to
+HOST = '127.0.0.1' # IP address of the host machine
+PORT = 5000 # Port to connect to
 
 nickname = input("Enter your nickname: ")
+key = None
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a TCP/IP socket
-client.connect((HOST, PORT))  # Connect to the server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a TCP/IP socket
+client.connect((HOST, PORT)) # Connect to the server
 
 def receive():
-    """Receive messages from the server."""
+    """Receive and decrypt messages from the server."""
+    global key
     while True:
         try:
-            message = client.recv(1024).decode()
+            if key is None:
+                # If this is the first message, receive the encryption key
+                key = client.recv(1024)
+                continue
+            encrypted_message = client.recv(1024)
+            message = decrypt_message(encrypted_message, key)
             print(message)
         except:
             print("An error occurred!")
@@ -21,10 +30,29 @@ def receive():
             break
 
 def write():
-    """Send messages to the server."""
+    """Encrypt and send messages to the server."""
+    global key
     while True:
-        message = f"{nickname}: {input('')}"
-        client.sendall(message.encode())
+        try:
+            message = f"{nickname}: {input('')}"
+            encrypted_message = encrypt_message(message, key)
+            client.sendall(encrypted_message)
+        except:
+            print("An error occurred!")
+            client.close()
+            break
+
+def encrypt_message(message, key):
+    """Encrypt a message using a symmetric encryption key."""
+    f = Fernet(key)
+    encrypted_message = f.encrypt(message.encode())
+    return encrypted_message
+
+def decrypt_message(encrypted_message, key):
+    """Decrypt an encrypted message using a symmetric encryption key."""
+    f = Fernet(key)
+    decrypted_message = f.decrypt(encrypted_message).decode()
+    return decrypted_message
 
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+import os
+from cryptography.fernet import Fernet
 
 HOST = '127.0.0.1'  # IP address of the host machine
 PORT = 5000  # Port to listen on
@@ -9,30 +11,55 @@ server.bind((HOST, PORT))  # Bind the socket to a specific address and port
 server.listen()  # Listen for incoming connections
 
 clients = []  # List to keep track of connected clients
+keys = {}  # Dictionary to keep track of encryption keys for each client
+
+
+def encrypt_message(message, key):
+    """Encrypt a message using a symmetric encryption key."""
+    f = Fernet(key)
+    encrypted_message = f.encrypt(message.encode())
+    return encrypted_message
+
+
+def decrypt_message(encrypted_message, key):
+    """Decrypt an encrypted message using a symmetric encryption key."""
+    f = Fernet(key)
+    decrypted_message = f.decrypt(encrypted_message).decode()
+    return decrypted_message
+
 
 def broadcast(message, sender):
     """Send a message to all connected clients except the sender."""
     for client in clients:
         if client != sender:
-            client.sendall(message)
+            key = keys[client]
+            encrypted_message = encrypt_message(message, key)
+            client.sendall(encrypted_message)
+
 
 def handle_client(client, address):
     """Handle a client connection."""
     print(f"New client connected: {address}")
     clients.append(client)
+    key = Fernet.generate_key()
+    keys[client] = key
+    client.sendall(key)
     while True:
         try:
-            message = client.recv(1024)
-            if message:
-                print(f"Received message: {message.decode()}")
+            encrypted_message = client.recv(1024)
+            if encrypted_message:
+                message = decrypt_message(encrypted_message, key)
+                print(f"Received message: {message}")
                 broadcast(message, client)
             else:
                 raise Exception('Client disconnected')
         except:
             print(f"Client disconnected: {address}")
             clients.remove(client)
+            del keys[client]
             client.close()
             break
+
 
 while True:
     client, address = server.accept()
